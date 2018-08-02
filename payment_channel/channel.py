57d16f757b0f95f1bf21f291f570d1894d6614db
Davide Gessa <gessadavide@gmail.com>
2018-08-02 10:05:36 +0200

    contract execution flow

--- payment_channel/channel.py
+++ payment_channel/channel.py
@@ -4,3 +4,20 @@ import requests
 
 NODE = "https://localhost:8732"
 
+# Compile the contract
+
+# Create two account for alice and bob with some tez
+
+# Alice deploys the contract with 10 XTZ
+
+# First interaction, a -> b : 1tez; generate the proof
+
+# Second interaction, a -> b : 2tez; generate the proof
+
+# Third interaction, a -> b : 3tez; generate the proof
+
+# Alice submits the proof for 6tez
+
+# Bob submits the proof for 6tez
+
+# Alice receives 4, bob receives 6
